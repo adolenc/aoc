a3522bb6c7e427a763f6fb413fa12fe585d3a13b
adolenc <mail@adolenc.me>
2022-12-03 15:03:29 +0100

    Add solution for day 3 2022

---
+++ 2022/03.hs
@@ -0,0 +1,17 @@
+-- https://adventofcode.com/2022/day/3
+import Data.Char
+import Data.List
+
+
+-- https://stackoverflow.com/a/12882583
+chunks n = takeWhile (not . null) . unfoldr (Just . splitAt n)
+
+main = do
+  inp <- lines <$> getContents
+  let findRepeats r = let (bot, top) = splitAt (length r `div` 2) r
+                      in head $ bot `intersect` top
+  let priority c = if c `elem` ['a'..'z']
+                   then ord c - ord 'a' + 1
+                   else ord c - ord 'A' + 27
+  print (sum $ map (priority . findRepeats) inp)
+  print (sum $ map (priority . head . foldr1 intersect) (chunks 3 inp))
