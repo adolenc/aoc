f9359f72bd68f5be99df276d141057045bc9762a
adolenc <mail@adolenc.me>
2022-12-14 23:22:35 +0100

    Add solution for day 14 2022

---
+++ 2022/14.hs
@@ -0,0 +1,36 @@
+-- https://adventofcode.com/2022/day/14
+import Data.List
+import Data.List.Split
+import qualified Data.Set as S
+
+
+type RockMap = S.Set (Int, Int)
+
+toRockMap :: String -> RockMap
+toRockMap = toMap . parseInput 
+  where
+    parseInput = map (map ((\(x:y:_) -> (read x, read y)) . splitOn ",") . splitOn " -> ") . lines
+    toMap = foldl' toMap' S.empty
+    toMap' m ((x,y):(x',y'):ps) = let rocks = S.fromList [(x,y) | x <- [min x x'..max x x'], y <- [min y y'..max y y']]
+                                  in toMap' (S.union m rocks) ((x',y'):ps)
+    toMap' m [p] = S.insert p m
+
+simulate :: Int -> RockMap -> Maybe RockMap
+simulate abyss = simulate' (500,0)
+  where
+    simulate' (x,y) m | S.member (500,0) m = Nothing
+                      | y >= abyss = Nothing
+                      | S.notMember   (x,y+1) m = simulate'   (x,y+1) m
+                      | S.notMember (x-1,y+1) m = simulate' (x-1,y+1) m
+                      | S.notMember (x+1,y+1) m = simulate' (x+1,y+1) m
+                      | otherwise = Just $ S.insert (x,y) m
+
+-- https://stackoverflow.com/a/35749543
+iterateMaybe f = unfoldr (fmap (\s -> (s,s)) . f)
+
+main = do
+  inp <- toRockMap <$> getContents
+  let abyss = maximum . map snd $ S.toList inp
+  print (length $ iterateMaybe (simulate abyss) inp)
+  let abyssFloor = S.fromList $ map (, abyss+2) [500-abyss*2..500+abyss*2]
+  print (length $ iterateMaybe (simulate (abyss + 10)) (S.union inp abyssFloor))
